16ae5b3550ab2527b423157e70df689b30ef1947
GekoNa92 <schastlivie2015@mail.ru>
2025-06-17 21:24:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const images = document.querySelectorAll(".image");
+const controls = document.querySelectorAll(".arrows");
+const dots = document.querySelectorAll(".dots")
+const nav = document.querySelectorAll(".nav")
+let imageIndex = 0;
+let dotsIndex = 0;
+let navIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+function showDots(index) {
+    dots[dotsIndex].classList.remove('active');
+    dots[index].classList.add('active');
+    dotsIndex = index;
+}
+function showNav(index) {
+    nav[navIndex].classList.remove('active');
+    nav[index].classList.add('active');
+    navIndex = index;
+}
+controls.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (e.classList.contains('left')) {
+            let index = imageIndex - 1;
+
+            if (index < 0) {
+                index = images.length -1;
+            }
+            show(index);
+            showDots(index);
+            showNav(index);
+        } else if (e.classList.contains('right')) {
+            let index =  imageIndex + 1;
+
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index);
+            showDots(index);
+            showNav(index);
+        }
+    })
+})
+show(imageIndex);
+
+
